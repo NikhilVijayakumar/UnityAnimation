7d5c29ecd4b4f52d50a32075e17cda6da254d297
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-11-10 20:08:22 +0530

    state and tranction

---
+++ Assets/StateAndTransction/Script/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = this.GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("isOpened", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("isOpened", false);
+    }
+
+
+}
