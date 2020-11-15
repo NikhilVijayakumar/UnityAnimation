de61c272c76a68de31a7b9f8557ec4b46328499e
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-11-15 10:29:15 +0530

    jump animation

---
+++ Assets/Scripts/TriggerGrabbing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGrabbing : MonoBehaviour {
+
+    public GameObject rootPos;
+
+    private void OnTriggerEnter(Collider other) {
+
+        other.GetComponentInParent<ClimbUp>().GrabEdge(rootPos.transform);
+    }
+}
