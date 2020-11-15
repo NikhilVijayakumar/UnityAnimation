a4086bdd7d31aa5b734d4b52b5c889e92e21c229
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-11-15 21:36:05 +0530

    blend

---
+++ Assets/Blend/Shape/BlendController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendController : MonoBehaviour
+{
+
+    private SkinnedMeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inflat = Input.GetAxis("Vertical");
+        meshRenderer.SetBlendShapeWeight(0, inflat * 100);
+    }
+}
---
+++ Assets/BlentTree/Script/BlentTreeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlentTreeController : MonoBehaviour {
+
+    float speed = 1.0f;
+    float rotationSpeed = 100.0f;
+    Animator anim;
+    float currentSpeed = 0.0f;
+   
+
+    void Start() {
+
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update() {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        currentSpeed += translation;     
+
+        anim.SetFloat("speed",currentSpeed);
+
+
+        transform.Rotate(0.0f, rotation, 0.0f);
+
+        if(translation != 0) {
+
+            anim.SetBool("isWalking", true);
+        } else {
+
+            anim.SetBool("isWalking", false);
+            currentSpeed = 0;
+        }
+    }
+}
